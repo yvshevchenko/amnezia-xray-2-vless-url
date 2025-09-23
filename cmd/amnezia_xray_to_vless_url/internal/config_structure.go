964998c6011f9494b068405c218ed229c53c02b7
Yaroslav Shevchenko <ys@teseract.com>
2025-09-23 14:25:11 +0300

    config json structure

---
+++ cmd/amnezia_xray_to_vless_url/internal/config_structure.go
@@ -0,0 +1,58 @@
+package internal
+
+type SourceConfig struct {
+	InboundsSettings []InboundEntity `json:"inbounds"`
+	LogSettings      `json:"log"`
+	OutboundSettings []OutboundEntity `json:"outbounds"`
+}
+
+type InboundEntity struct {
+	Listen   string                `json:"listen"`
+	Port     string                `json:"port"`
+	Protocol string                `json:"protocol"`
+	Settings InboundEntitySettings `json:"settings"`
+}
+
+type InboundEntitySettings struct {
+	Udp bool `json:"udp"`
+}
+
+type LogSettings struct {
+	Loglevel string `json:"loglevel"`
+}
+
+type OutboundEntity struct {
+	Protocol       string                       `json:"protocol"`
+	Settings       OutboundEntitySettings       `json:"settings"`
+	StreamSettings OutboundEntityStreamSettings `json:"streamSettings"`
+}
+
+type OutboundEntitySettings struct {
+	Vnext []VnextEntity `json:"vnext"`
+}
+
+type VnextEntity struct {
+	Address string               `json:"address"`
+	Port    string               `json:"port"`
+	Users   []UserSettingsEntity `json:"users"`
+}
+
+type UserSettingsEntity struct {
+	Encryption string `json:"encryption"`
+	Flow       string `json:"flow"`
+	Id         string `json:"id"`
+}
+
+type OutboundEntityStreamSettings struct {
+	Network         string          `json:"network"`
+	RealitySettings RealitySettings `json:"realitySettings"`
+	Security        string          `json:"security"`
+}
+
+type RealitySettings struct {
+	Fingerprint string `json:"fingerprint"`
+	PublicKey   string `json:"publicKey"`
+	ServerName  string `json:"serverName"`
+	ShortId     string `json:"shortId"`
+	SpiderX     string `json:"spiderX"`
+}
